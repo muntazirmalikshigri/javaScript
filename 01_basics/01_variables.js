fe6510675220115aaaea6a8523ff56c854728854
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-08-24 16:36:01 +0000

    no commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1223;
+let accountEmail = "muntazirshigri@gmail.com";
+var accountPassword = "12345";
+accountCity = "karachi";
+//accountId = 2; not allowed to change
+accountPassword = "4321";
+accountEmail = "malik@gamil.com";
+accountCity = "lahore";
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
