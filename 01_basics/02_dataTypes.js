88f048acf4519a588228fde2edce460bf28c1f71
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-08-27 16:39:42 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+//"use streat"; //treat all js code as newer version
+//alert(3+3); // we are using node js, not browser
+console.log("malik");
+// number == 3;
+//  bigint
+// string => "";
+// bolean
+// null => standalone value
+// undefined
+
+
+// object
+
+
+console.log(typeof null);
+console.log9
