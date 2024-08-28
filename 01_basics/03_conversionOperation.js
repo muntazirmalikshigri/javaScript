afc87f7a088845317e483140428dab0bd53e14c2
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-08-28 21:40:29 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "malik";
+let isLoggedIn =" a";
+//const {score} = req.body
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(typeof (valueInNumber));
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let string = 77;
+let valueInSting = String(string);
+console.log(typeof valueInSting);
