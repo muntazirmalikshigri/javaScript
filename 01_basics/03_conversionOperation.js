ae535b78d241fa5d42c9fa22d163cebf3bac422e
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-09-01 00:05:08 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,10 +5,34 @@ let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
 // console.log(typeof (valueInNumber));
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 let string = 77;
 let valueInSting = String(string);
-console.log(typeof valueInSting);
+// console.log(typeof valueInSting);
+
+//******************operation**********//
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str = "Hello";
+let str1 = "Muntazir";
+
+let str2 = str + str1;
+// console.log(str2)
+
+// console.log('1'+2);
+// console.log(1+'2');
+// console.log('1'+2+2);
+// console.log(1+2+'2');
+
+console.log(+true);
+console.log(+"");
+
+let num,num1,num2;
+num=num1=num2=2+2;
+console.log(num1);
+
