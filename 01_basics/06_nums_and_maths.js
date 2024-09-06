a9be45fd5da2d849d386e87301fd32d28553fa04
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-09-06 21:57:14 +0000

    numbers and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+const score = 100;
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+let num = 1000000;
+console.log(num.toLocaleString());
+const otherNumbers = 123.8966;
+console.log(otherNumbers.toPrecision(5));
+
+//**************Maths*********//
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(2.2));
+// console.log(Math.floor(3.9));
+// console.log(Math.round(3.9));
+// console.log(Math.round(Math.random()*10+1));
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(min - max + 1)) * min);
