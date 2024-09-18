20a2cd100bad8269161cc4d591141f49cd216dca
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-09-18 22:08:51 +0000

    object in js

---
+++ 02_basics/04_object.js
@@ -0,0 +1,45 @@
+const tinderUser = new Object();
+console.log(typeof tinderUser);
+tinderUser.id='m1122';
+tinderUser.name='malik';
+tinderUser.level=5;
+console.log(tinderUser);
+
+const regularUser = {
+    id:'1122',
+    fullname:{
+        username:{
+            firstname:"muntu",
+            lastname:'malik'
+        }
+    }
+};
+console.log(typeof regularUser.fullname.username.firstname);
+console.log(regularUser.fullname.username);
+const obj1={1:"a",2:"b"};
+const obj2={1:"c",2:"d"};
+// const obj3={obj1,obj2};
+// const obj3=Object.assign(obj1,obj2);
+// console.log(obj3);
+// const obj4={...obj1,...obj2,...obj3};
+// console.log(obj4)
+
+
+const user = [
+    {
+id:1122,
+name:"fakhar"
+},
+{
+ id:1133,
+ name:"sohail"   
+},
+{
+    id:144,
+    name:"malik"
+}
+];
+console.log(user[0].name);
+console.log(Object.values(tinderUser))
+console.log(tinderUser.hasOwnProperty("id"));
+
