6ef6a4f182391a407b82d7b258699782aa8cc7d8
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-09-22 20:57:41 +0000

    function hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,5 +7,45 @@ if(true){
     
 }
 //console.log(a);
-console.log(b);
+// console.log(b);
 //console.log(c);
+
+function one(){
+    const username = "fakhar"
+
+    function two(){
+    const website = "youtube"
+
+    console.log(username)
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "malik"
+    if(username==="malik"){
+        const website = "youtube"
+        console.log(website)
+    }
+    // console.log(website)
+    console.log(username)
+}
+// console.log(username)
+
+//+++++++++++++++++++++++++Intresting++++++++++++++++++++++++
+
+console.log(addOne(4));
+function addOne(num){
+    return num + 1;
+}
+
+// addOne(4);
+ 
+// addTwo(4);
+addTwo = function(num){
+    return num + 2;
+}
+
+// addTwo
