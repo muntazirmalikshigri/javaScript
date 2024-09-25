9f5d4611d55be112d15ed4cf8a43ff8ff8fbbfc4
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-09-25 22:31:00 +0000

    control_flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+// if
+// if(true){
+// console.log("true")
+// }
+
+// if(2<=2){
+//     console.log("Yes")
+// }
+
+// const temperature = 40;
+// if(temperature<50){
+//     console.log("less than 50")
+// }else{
+// console.log("greater than 50")
+// }
+const score = 200;
+if(score>100){
+    const power = "fly"
+    console.log(`power is: ${power}`);
+}
