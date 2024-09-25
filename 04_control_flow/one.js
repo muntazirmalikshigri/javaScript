9a63c81d372fbf6866aa9e5ed3dfde608cbadca0
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-09-25 23:14:24 +0000

    control_flow in js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -13,12 +13,27 @@
 // }else{
 // console.log("greater than 50")
 // }
-const score = 200;
-if(score<100){
-    let power = "fly"
-    console.log(`power is: ${power}`);
-}
-console.log(`power is: ${power}`);
+// const score = 200;
+// if(score<100){
+//     let power = "fly"
+//     console.log(`power is: ${power}`);
+// }
+// console.log(`power is: ${power}`);
 
-const balance = 1000;
-if(balance>500) console.log("test");
+// const balance = 1000;
+// // if(balance>500) console.log("test");
+// if(balance<500){
+//     console.log("yes,less than 500");
+// }else if(balance<600){
+//     console.log("Yes,less than 600");
+// }else if(balance<700){
+//     console.log("Yes, less than 700");
+// }else{
+//     console.log("default value");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
