8ef1cd67b98050946635fc64ea3b7bd20116c8f1
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-09-27 15:24:54 +0000

    while and do while in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+//    while loop in js
+let i =0;
+while(i<=10){
+    console.log(`the value of i is: ${i}`);
+    i++;
+}
+let arr=["fakhar",
+    "malik","sohail"
+];
+let j=0;
+while(j<arr.length){
+    console.log(arr[j]);
+    j++;
+}
+
+//  do while loop
+
+let k =0;
+do{
+    console.log(k);
+    k++;
+}while(k<=5);
