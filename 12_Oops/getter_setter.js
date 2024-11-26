1cd267a1a18c7659bacd395052191a2329753063
muntazirmalikshigri <160231293+muntazirmalikshigri@users.noreply.github.com>
2024-11-26 21:41:34 +0000

    getter and setter

--- 12_Oops/getter_setter.js
+++ 12_Oops/getter_setter.js
@@ -1,8 +1,24 @@
 class User{
     constructor(email , password)
+    {
     this.email = email
     this.password = password
+    }
+
+    get email(){
+        return this.email.toUpperCase()
+    }
+    set email(value){
+        return this._email = value
+    }
+    get password(){
+   return `${this._password}malik`
+    }
+
+    set password(value){
+     this._password = value
+    }
 }
 
-const hitesh = User("malik@gmail.com","123");
+const malik =  User("malik@gmail.com","123");
 console.log(malik.password)
